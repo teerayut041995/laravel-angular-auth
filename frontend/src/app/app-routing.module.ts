d46fc353f81297de7ebc10fd0592bf7c2258f238
teerayut041995 <teerayut041995@gmail.com>
2019-02-26 11:49:58 +0700

    Angular Routings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: ProfileComponent},
+  {path: 'auth/login' , component: LoginComponent},
+  {path: 'auth/signup' , component: SignupComponent},
+  {path: 'profile' , component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
